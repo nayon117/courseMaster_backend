c1a2eeb8c1b182a0ad281da26e9a428b9cb9519b
nayon117 <hasibul.nayon1@gmail.com>
2025-12-03 16:00:13 +0600

    setup express and mongodb

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import errorHandler from './middlewares/error.js';
+
+const app = express();
+
+// middleware
+app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(express.json());
+
+
+// routes
+
+
+
+app.use(errorHandler);
+
+// test route
+app.get('/', (req, res) => {
+  res.send('CourseMaster API is running');
+});
+
+// connect db and start server
+const start = async () => {
+  await connectDB();
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+};
+
+start();
